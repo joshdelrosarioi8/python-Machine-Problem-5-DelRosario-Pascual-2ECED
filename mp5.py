7196a3d9a54d183a51656437c25977421fee7823
joshdelrosarioi8 <57487025+joshdelrosarioi8@users.noreply.github.com>
2019-12-08 23:49:28 +0800

    Add files via upload

---
+++ mp5.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+f=np.arange(0,100)
+for n in range(0,100):
+    if f[n]<=9:
+      f[n]=(f[n]**2)-7
+    elif f[n]>=10:
+      f[n]=f[n-10]
+      plt.stem(f,use_line_collection=True)
+      plt.show()
+      plt.figure()
+      plt.xlabel('x-axis')
+      plt.xlabel('y-axis')
+      plt.title('problem 1')
